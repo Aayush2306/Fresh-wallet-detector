1e2b30e094b0c4ade4d20e24eae2627518680f0b
Aayush2306 <64463251+Aayush2306@users.noreply.github.com>
2023-01-28 15:39:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const freeKey = "EK-cgMkq-f79VYYW-u1JY5";
+const freeKey2 = "EK-pfZvx-FosL1Um-w77mG";
+const freeKey3= "EK-22krG-VXcvgYY-jyyuo"
+const freeKey4= "EK-6wZSf-zBCHfyy-ESj99"
+const freekey5= "EK-af3uZ-LcuHf73-d1WJu"
+const freekey6="EK-8RsfJ-ckCnNW5-ddbmS"
+const address = "0x2e311fAeb44d4781f932eEd293ED59Fd629D1ba5"
+const apiUrl = `https://api.ethplorer.io/getTopTokenHolders/${address}?apiKey=${freeKey}&limit=60`;
+
+let num = 0
+let sum =0
+async function getapi(url) {
+    
+    const response = await fetch(url);
+    var data = await response.json();
+   const maal = data.holders;
+   const asliMaal = maal.map(obj => obj.address) 
+
+  asliMaal.forEach((obj,i) => {
+    
+
+    async function getTx(freeKey) {
+
+      const url = `https://api.ethplorer.io/getAddressInfo/${obj}?apiKey=${freeKey}&showETHTotals=false`
+
+            const res = await fetch(url)
+ 
+            var data = await res.json();
+            totalTx = data.countTxs
+            if(totalTx< 10) {
+               sum = sum+ 1
+               console.log(data.address)
+            }
+  
+           const percentage = (sum/asliMaal.length)*100
+           console.log(`Total New Wallet = ${sum} and total wallet checked ${asliMaal.length}`)
+    
+    }
+    if(i<9) {
+        getTx(freeKey)
+    }
+     if(i>=10&& i<20) {
+         getTx(freeKey2)
+     }
+      if(i>=20 && i<30) {
+        getTx(freeKey3)
+      }
+      if(i>=30 && i<40) {
+         getTx(freeKey4)
+      }
+      if(i>=40 && i<50) {
+        getTx(freekey5);
+      }
+      if(i>=50 && i<60) {
+        getTx(freekey6)
+      }
+  })
+}
+
+//getapi(apiUrl)
+
+
+const grab = document.getElementById("ca")
+const btn = document.getElementsByClassName("btn")
+
+console.log(grab)
+
+btn[0].addEventListener("click", ()=> {
+  const ca = grab.value
+  console.log(ca)
+});
